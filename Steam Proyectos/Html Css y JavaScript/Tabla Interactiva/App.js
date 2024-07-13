728a0be2fa631da306f1d2b7c0a20545aa05550c
Andy c <81699558+andycr123@users.noreply.github.com>
2024-07-12 19:28:53 -0500

    Add files via upload

---
+++ Steam Proyectos/Html Css y JavaScript/Tabla Interactiva/App.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelectorAll('[data-id]');
+const contents = document.querySelectorAll('[data-content]');
+let id = 0;
+
+tabs.forEach(function (tab) {
+    tab.addEventListener('click', function () {
+        tabs[id].classList.remove('active');
+        tab.classList.add('active');
+        id = tab.getAttribute('data-id');
+        contents.forEach(function (box) {
+            box.classList.add('hide');
+            if (box.getAttribute('data-content') == id){
+                box.classList.remove('hide');
+                box.classList.add('show');
+            }
+        });
+    });
+});
